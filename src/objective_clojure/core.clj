3ff4690e0a0b4e37b186fd67cd4ac06598733949
Lyndon Tremblay <humasect@gmail.com>
2012-02-10 22:57:49 -0500

    test file added

--- src/objective_clojure/core.clj
+++ src/objective_clojure/core.clj
@@ -1,23 +1,7 @@
 (ns objective-clojure.core)
 
-(def testfile "/Users/humasect/hv/experiments/objective-clojure-gamelike-actor.lisp")
-
-(defn load-forms [s])
-
-(defn test []
-  (with-in-str (slurp filename)
-    (let [eof nil]
-      (while (not eof) ))))
-
-(defn test2 []
-  (with-in-str (slurp testfile)
-    (loop []
-      (when (not (read nil nil))))))
-
-(defn load-db [filename]
-  (with-in-str (slurp filename) (read)))
-
-;;;;;
+;;(def testfile "/Users/humasect/hv/experiments/objective-clojure-gamelike-actor.lisp")
+(def testfile "gamelike-actor.oclj")
 
 (defn read-forms [stream]
   (when-let [form (read stream false nil false)]
